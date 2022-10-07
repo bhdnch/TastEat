29f5e6ca587385057723834330fe28cbe2862ab4
Bohdan <bohdanbilous.l@gmail.com>
2022-10-07 14:19:42 +0200

    commit

--- js/swiper-init.js
+++ js/swiper-init.js
@@ -1,26 +1,18 @@
 const swiper = new Swiper(".swiper", {
-   slidesPerView: "auto",
-   // Optional parameters
-   loop: false,
-   // spaceBetween: 20,
-   // centeredSlides: true,
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-   // If we need pagination
-   pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-   },
-
-   breakpoints: {
-      640: {
-         slidesPerView: 1,
-         spaceBetween: 20,
-      },
-   },
-
-   // Navigation arrows
-   // navigation: {
-   //    nextEl: ".swiper-button-next",
-   //    prevEl: ".swiper-button-prev",
-   // },
+  breakpoints: {
+    640: {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+    },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  },
 });
